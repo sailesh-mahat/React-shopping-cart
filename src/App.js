545c21de85237e637cf48975625565a422a3e112
sailesh-mahat <sailesharsenal@gmail.com>
2021-02-11 11:47:23 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          Product List
+        </main>
+        <footer>
+          All Rights Reserved
+        </footer>
     </div>
   );
 }
